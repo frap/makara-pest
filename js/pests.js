489c3f4f29359773e52c8eac2e9d183323572f56
Gas <gas@tuatara.red>
2022-09-29 10:28:51 +1300

    small typos

--- js/pests.js
+++ js/pests.js
@@ -28,23 +28,23 @@ var esri_NatGeoWorldMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/res
 	maxZoom: 16
 });
 
-var CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+var cycle_OSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+var open_TopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
-var Thunderforest_OpenCycleMap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
+var thunderforest_OpenCycleMap = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
 	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	apikey: '14d1c23f2adc41828d0519f830365211',
 	maxZoom: 22
   });
 
-var Stamen_TerrainBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
+var stamen_TerrainBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
@@ -57,12 +57,12 @@ var baseLayers = {
 	  "ESRI World Imagery": esri_WorldImagery,
 	  "ESRI World Terrain": esri_WorldTerrain,
 	  "ESRI National Geographic": esri_NatGeoWorldMap,
-    "Cycle OSM": CyclOSM,
-    "ThunderForest Cycle": Thunderforest_OpenCycleMap,
-    "Statem Terrain": Statem_TerrainBackground
+    "Cycle OSM": cycle_OSM,
+    "ThunderForest Cycle": thunderforest_OpenCycleMap,
+    "Statem Terrain": statem_TerrainBackground
 };
 
 // add the layer to the map
-map.addLayer(CyclOSM);
+map.addLayer(cycle_OSM);
 
 //L.control.layers(baseLayers).addTo(map);
